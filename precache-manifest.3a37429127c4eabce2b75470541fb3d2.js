faa0ee55e43806f78f63c2c4d4f8d92762442601
Giacomo Sorbi <giacomo.sorbi@bjss.com>
2019-07-07 20:15:39 +0100

    Updates

---
+++ precache-manifest.3a37429127c4eabce2b75470541fb3d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39eba7e37f48d0ae083f2a7e4ec4896",
+    "url": "/coin-map/index.html"
+  },
+  {
+    "revision": "5c8c20dcb1aca00c84bb",
+    "url": "/coin-map/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "49d0194fc61bf7709635",
+    "url": "/coin-map/static/js/2.c4a13e6f.chunk.js"
+  },
+  {
+    "revision": "5c8c20dcb1aca00c84bb",
+    "url": "/coin-map/static/js/main.382fa049.chunk.js"
+  },
+  {
+    "revision": "fbbb67108f39c60d818a",
+    "url": "/coin-map/static/js/runtime~main.1ba1251b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coin-map/static/media/logo.5d5d9eef.svg"
+  }
+]);
